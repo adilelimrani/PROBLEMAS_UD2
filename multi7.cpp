29bc66c2d8ca3e627564002c4b0b4558aa9e5cf0
adilelimrani <adilelimrani@gmail.com>
2015-12-18 15:09:59 +0100

    Create multi7.cpp

---
+++ multi7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	int numero=1;
+
+	while (numero <=10) {
+		cout << "7 X " << numero << " = " << 7*numero << endl;
+		numero=numero+1;
+	}
+}
