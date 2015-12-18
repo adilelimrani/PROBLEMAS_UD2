ccb4cd4cfa51ff08b49dbc9cc93576f0e624b19c
adilelimrani <adilelimrani@gmail.com>
2015-12-18 15:13:37 +0100

    Create tabla7.cpp

---
+++ tabla7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int sentencia=0;
+
+	do {
+
+		cout << "7 x " << sentencia<< "= "<< 7*sentencia << endl;
+		sentencia=sentencia+1;
+	} while (sentencia <=10);
+}
