c2ac966fa952e226c4ea1abc96d32cf0acb734de
adilelimrani <adilelimrani@gmail.com>
2015-12-18 15:12:37 +0100

    Create simpsons2.cpp

---
+++ simpsons2.cpp
@@ -0,0 +1,15 @@
+// Ejercicio de los simpsons con "While"
+
+
+#include <iostream>
+using namespace std;
+int main() {
+
+	int numero=1;
+
+	while (numero <=10) {
+		cout << numero << ") ";
+		cout << "No debo copiar en clase" << endl;
+		numero=numero+1;
+	}
+}
