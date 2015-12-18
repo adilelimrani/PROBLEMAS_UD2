d078f7be565170896eb375f0aa65cc6db7840b05
adilelimrani <adilelimrani@gmail.com>
2015-12-18 15:12:10 +0100

    Create simpsons.cpp

---
+++ simpsons.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int sentencia=0;
+
+	do {
+
+		cout << "no debo copiar en clase" << endl;
+		sentencia=sentencia+1;
+	} while (sentencia <=10);
+}
