62871fd385bdf358fabb3da8794cde5a5fe85a8a
adilelimrani <adilelimrani@gmail.com>
2015-12-18 14:25:23 +0100

    Create 22-satelitefor.cpp

---
+++ 22-satelitefor.cpp
@@ -0,0 +1,17 @@
+//satelite recibe 10 datos y se para.
+
+
+
+#include <iostream>
+using namespace std;
+int main() {
+
+	int numero=0;
+
+	for (int cuenta=1 ;cuenta<=10 ; cuenta++){
+		cout << "introducir datos: ";
+		cin >> numero;
+	}
+
+	return 0;
+}
